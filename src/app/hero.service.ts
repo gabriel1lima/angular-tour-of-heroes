d199b683e09fd061680dd3fe5c67111bc82f2527
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-03-06 16:27:26 -0300

    Metodos de add Herói (fix conflito 2)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,29 +8,18 @@ import { MessageService } from "./message.service";
   providedIn: "root"
 })
 export class HeroService {
-<<<<<<< HEAD
-  HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-  ];
-
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(this.HEROES);
-=======
   id = 13;
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
->>>>>>> 302c9dfd87e3a06323bb7b8f3563578f1af9bace
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(this.HEROES.find(hero => hero.id === id));
+    return of(HEROES.find(hero => hero.id === id));
   }
 
   deleteHero(hero: Hero) {
@@ -38,13 +27,9 @@ export class HeroService {
   }
 
   addHero(hero: Hero) {
-<<<<<<< HEAD
-    this.HEROES.push(hero)
-=======
     hero.id = this.id;
     this.id++;
     HEROES.push(hero);
->>>>>>> 302c9dfd87e3a06323bb7b8f3563578f1af9bace
   }
 
   constructor(private messageService: MessageService) {}
