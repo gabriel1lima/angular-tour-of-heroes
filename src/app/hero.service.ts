79c67eac77cdd93d152220ee017e3e21e91ba566
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-03-06 16:21:18 -0300

    Metodos de add Herói

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,22 +8,24 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
-  
+  HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+  ];
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return of(this.HEROES);
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return of(this.HEROES.find(hero => hero.id === id));
   }
 
   addHero(hero: Hero) {
-    HEROES.push(hero)
-    console.log(HEROES)
+    this.HEROES.push(hero)
   }
 
   constructor(private messageService: MessageService) { }
